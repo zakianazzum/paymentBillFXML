5cac4be59e4a82831843a400f246940f4341f28c
zakianazzum <zakianazzumgodhuli@gmail.com>
2024-07-02 00:07:57 +0600

    Added clearing ibput boxes after clicking cart button and using array list

--- src/main/java/com/example/mitermexam_2130086_sec6/MidtermVController.java
+++ src/main/java/com/example/mitermexam_2130086_sec6/MidtermVController.java
@@ -41,22 +41,16 @@ public class MidtermVController
     private ComboBox<Integer> quantityComboBox;
     @javafx.fxml.FXML
     private TableColumn<CartItem, Integer> summaryQuantityTableColumn;
-
+    @FXML
+    private Label totalAmountShowLabel;
 
     private ArrayList<CartItem> cartList = new ArrayList<>();
 
-    private Map<String, ProductInfo> productInfoMap = new HashMap<>();
+    private String[] productNames = {"Arong Dairy Milk", "Butter", "Cheese", "Honey", "Oats",
+                                            "PlayStation 5"};
+    private float[] unitPrices = {50.0f, 45.0f, 65.0f, 75.0f, 80.0f, 30000.0f };
+    private int[] vatRates = {1, 2, 4, 7, 3, 5 };
 
-
-    private static class ProductInfo {
-        float unitPrice;
-        int vatRate;
-
-        ProductInfo(float unitPrice, int vatRate){
-            this.unitPrice = unitPrice;
-            this.vatRate = vatRate;
-        }
-    }
 
 
     @FXML
@@ -64,10 +58,7 @@ public class MidtermVController
         quantityComboBox.getItems().addAll(1,2, 3, 4, 5, 6, 7, 8, 9, 10);
         vatToConsiderComboBox.getItems().addAll(1,2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-        productInfoMap.put("Arong Dairy Milk", new ProductInfo(50, 1));
-
-        productListCombobox.getItems().addAll(productInfoMap.keySet());
-
+        productListCombobox.getItems().addAll(productNames);
         productListCombobox.setOnAction(this::onProductSelected);
 
         summaryProductTableColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getProductName()));
@@ -86,13 +77,17 @@ public class MidtermVController
     public void checkOutButton(ActionEvent actionEvent) {
         cartDetailTableView.getItems().clear();
         cartDetailTableView.getItems().addAll(cartList);
-
+        float totalAmount = 0;
+        for (CartItem item : cartList){
+            totalAmount += item.getTotalAmount();
+        }
+        totalAmountShowLabel.setText(String.valueOf(totalAmount));
     }
 
     @javafx.fxml.FXML
     public void addProductCartButton(ActionEvent actionEvent) {
         String selectedProduct = productListCombobox.getValue();
-        ProductInfo info = productInfoMap.get(selectedProduct);
+        int index = findProductIndex(selectedProduct);
         int quantity = quantityComboBox.getValue();
 
         CartItem existingItem = null;
@@ -106,21 +101,33 @@ public class MidtermVController
             existingItem.setQuantity(existingItem.getQuantity() + quantity);
         }
         else{
-            CartItem newItem = new CartItem(selectedProduct, info.unitPrice,quantity,info.vatRate);
+            CartItem newItem = new CartItem(selectedProduct,unitPrices[index],quantity,vatRates[index]);
             cartList.add(newItem);
         }
+        // Clear all input boxes
+        productListCombobox.setValue(null);
+        UnitPriceLabel.setText("");
+        predefinedVatLabel.setText("");
+        quantityComboBox.setValue(null);
 
+    }
 
-
+    private int findProductIndex(String productName){
+        for (int i = 0; i < productNames.length; i++){
+            if (productNames[i].equals(productName)){
+                return i;
+            }
+        }
+        return -1;
     }
 
     @javafx.fxml.FXML
     public void onProductSelected(ActionEvent actionEvent) {
         String selectedProduct = productListCombobox.getValue();
-        ProductInfo info = productInfoMap.get(selectedProduct);
-        if (info != null) {
-            UnitPriceLabel.setText(String.valueOf(info.unitPrice));
-            predefinedVatLabel.setText(String.valueOf(info.vatRate));
+        int index = findProductIndex(selectedProduct);
+        if (index != -1) {
+            UnitPriceLabel.setText(String.valueOf(unitPrices[index]));
+            predefinedVatLabel.setText(String.valueOf(vatRates[index]));
         }
     }
 
